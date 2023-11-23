ae78861c43d611037c4484c9bc2549d88d92f853
lynn <cedriccolinc@yahoo.com>
2023-11-23 16:30:09 +0100

    Connexion à la base de donnée
    - **Nouvelle dépendance :** `pymysql`
    - Ajout de constantes dans le script python pour se connecter à la base de donnée
    - Ajout d'une fonction pour obtenir une connexion à la base de donnée
    - Ajout de la fonction `teardown_db` qui ferme les connexions à la base de donnée (pour éviter les memory leaks)

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import pymysql.cursors
 
 app = Flask(__name__)
 app.secret_key = "e%3v*/=Nj8Zbzz=$bqr1DA$BM7V6/sgWiFD7/NUa6F$psx3wZC6zr~C8MxGAM)#F"
 
 # TODO Voir les noms de variables à mettre :/
 # FIXME Il faut modifier quand vous êtes sur vos machines
-HOST = ""
-PASSWORD = ""
-DATABASE = ""
+HOST = "localhost"
+USER = "lynn"
+PASSWORD = "Pusyux8484"  # Ce mot de passe est évidemment faux, je ne suis pas fou
+DATABASE = "hydrobus_belfortain"
+
+
+def get_db():
+    """
+    Establish and return a connection to the MySQL database.
+    This function uses the PyMySQL library to establish a connection,
+    with connection parameters such as the host, user, password, and database specified.
+
+    The connection uses the 'utf8mb4' charset and DictCursor for cursor-class, which returns rows
+    from the database as dictionaries instead of tuples.
+
+    If a connection (denoted as 'db') does not exist in the current application context (g), a new
+    connection is established.
+
+    Once established, the connection is saved in the application context to be reused
+    on subsequent database queries in the same request.
+
+    :return: The MySQL database connection
+    """
+    if 'db' not in g:
+        g.db = pymysql.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE,
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    return g.db
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/')
